ea98a54c6e6956d2fcd5b09a169687746c38aee9
Ayushraj2141 <ayushrajconnect@gmail.com>
2025-06-11 08:44:31 +0000

    thied

--- Python_basics/4_list_tuples/list.py
+++ Python_basics/4_list_tuples/list.py
@@ -1,2 +1,5 @@
-# list is container which contains all types of datatypes 
-listt = [1,2,"string", 54.78,53,43,""]
+# list is container which stores all types of data types 
+listt = [1,2,"string", 54.78,53,43,"hello"]
+print(listt[2][5])
+listt[0]=2
+print(listt[0]) # lists are mutable unlike strings 
